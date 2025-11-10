80e59f2739b790f0da17f702190420064c739baa
psyts20 <psyts20@nottingham.ac.uk>
2025-11-10 13:59:10 +0000

    Add Valid Parentheses Attempt 1

--- stacks/valid_parentheses.py
+++ stacks/valid_parentheses.py
@@ -9,3 +9,40 @@ The input string s is valid if and only if:
 
 Return true if s is a valid string, and false otherwise.
 '''
+
+# Attempt 1: Stack Backtracking
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution0:
+    def isValid(self, s: str) -> bool:
+        # Create stacks to hold each character
+        stack = []
+        open_brackets = ['(', '{', '[']
+        
+        for character in s:
+            # If the character is an open bracket, add it to the stack.
+            if character in open_brackets:
+                stack.append(character)
+            # If the character is a closed bracket, compare it to the top item on the stack
+            # and if it is the correct closing character, pop the stack.
+            elif len(stack) > 0:
+                peek = stack[-1]
+                if peek == '(' and character == ')':
+                    stack.pop()
+                elif peek == '{' and character == '}':
+                    stack.pop() 
+                elif peek == '[' and character == ']':
+                    stack.pop() 
+                else:
+                    return False
+            else:
+                return False
+        # If the stack is empty, the correct number sequence of brackets has occured.
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+
+
+        
