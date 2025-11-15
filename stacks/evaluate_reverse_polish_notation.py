5f403df443cd660309a712b14caa0c25d120a811
psyts20 <psyts20@nottingham.ac.uk>
2025-11-15 11:54:23 +0000

    Add Model Solution

--- stacks/evaluate_reverse_polish_notation.py
+++ stacks/evaluate_reverse_polish_notation.py
@@ -47,3 +47,26 @@ class Solution0:
         
         # Return the only value left on the stack.
         return stack.pop()
+    
+    
+    
+# Model Answer: Stack Evaluation
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+class SolutionM:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
