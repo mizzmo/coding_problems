44fe8e4c4d6717b34b39c1625dfc6c4fea0aaf66
psyts20 <psyts20@nottingham.ac.uk>
2025-11-15 11:22:34 +0000

    Move File, Update Stacks Landing

---
+++ stacks/evaluate_reverse_polish_notation.py
@@ -0,0 +1,12 @@
+'''Evaluate Reverse Polish Notation
+Solved
+
+You are given an array of strings tokens that represents a valid arithmetic expression in Reverse Polish Notation.
+
+Return the integer that represents the evaluation of the expression.
+
+    The operands may be integers or the results of other operations.
+    The operators include '+', '-', '*', and '/'.
+    Assume that division between integers always truncates toward zero.
+    
+'''
