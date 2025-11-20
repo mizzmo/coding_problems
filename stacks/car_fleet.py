33a6c521f9fb6b736102fdb40a87e762a67cec9c
psyts20 <psyts20@nottingham.ac.uk>
2025-11-20 19:16:24 +0000

    Add Attempt 2

--- stacks/car_fleet.py
+++ stacks/car_fleet.py
@@ -67,6 +67,49 @@ class Solution0:
         # Return length of the final stack.
         return len(fleets)
 
+
+# Attempt 2: Refined Stack
+# Time complexity: O(n log n)
+# Space complexity: O(n)
+class Solution1:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # Keep a stack of the fleets
+        fleets = []
+        # Find the order to process the cars
+        order = {}
+        for i in range(len(position)):
+            order[position[i]] = speed[i]
+        
+        # Get the correct order of positions by sorting in reverse.
+        position.sort(reverse = True)
+        leading_iterations = 0
+        for car_pos in position:
+            # Get current car attributes
+            car_spd = order[car_pos]
+            # Calculate iterations from end.
+            iterations = (target - car_pos) / car_spd
+            
+            if car_pos == position[0]:
+                fleet = [car_pos]
+                leading_iterations = iterations
+            else:
+                # Work out if the car meets the leading car before the destination.
+                if iterations <= leading_iterations:
+                    # Add to current fleet.
+                    fleet.append(i)
+                else:
+                    # If it does not meet, it becomes the leader of a new fleet
+                    # Store previous fleet
+                    fleets.append(fleet)
+                    # Start a new fleet
+                    fleet = [car_pos]
+                    leading_iterations = iterations
+            
+        # Once all cars are in a fleet, add the final fleet.
+        fleets.append(fleet)
+        
+        return len(fleets)
+
             
 # Model Solution 1: Stack 
 # Time complexity: O(n log n)
